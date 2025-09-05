06f9a10f257aa63d3b0e3bba84b0608bdbc50483
Selene <selenenoma58@gmail.com>
2025-09-04 23:29:12 -0300

    back.php2

--- back.php
+++ back.php
@@ -3,9 +3,9 @@
     $usuario = "admin";
     $pass = "1234";
 
-    if($usuario==$_POST['usuario'] && $pass ==$_POST['pass'])
+    if($usuario==$_POST['usuario'] && $pass ==$_POST['pass']){
         echo "ok";
-    else{
+    }else{
         echo "error";
         }
 ?>
